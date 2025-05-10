930e109bcd0f2ececf40019cb7ae4c9cc13725fb
Morgan Kenyon <morganwkenyon@gmail.com>
2025-05-09 21:14:02 -0500

    Adding the ability to insert into the sqlite database

--- src/FalcoTemplate.Api/Program.fs
+++ src/FalcoTemplate.Api/Program.fs
@@ -1,12 +1,74 @@
 module FalcoTemplate.Api
 
+open Dapper.FSharp
+open Dapper.FSharp.SQLite
 open DbUp
 open Falco
 open Falco.Markup
 open Falco.Routing
 open Microsoft.AspNetCore.Builder
-open System.Data.SQLite
-// ^-- this import adds many useful extensions
+
+///Model stuff
+
+type NewUser =
+    {
+        FirstName : string
+        LastName : string
+        Phone: string
+    }
+
+type User =
+    {
+        UserId : int32
+        FirstName : string
+        LastName : string
+        Phone: string
+    }
+
+type DbUser =
+    {
+        user_id : int32
+        first_name : string
+        last_name : string
+        phone : string
+    }
+
+let usersTable = table'<DbUser> "users"
+///DB Stuff
+let insertUser (newUser: NewUser) =
+    task {
+        use conn = new System.Data.SQLite.SQLiteConnection("Data Source=./falco.db")
+        conn.Open()
+
+        let user =
+            {
+                user_id = 0
+                first_name = newUser.FirstName
+                last_name = newUser.LastName
+                phone = newUser.Phone
+            }
+
+        let! insertedCount =
+            insert {
+                for p in usersTable do
+                value user
+                excludeColumn p.user_id
+            } |> conn.InsertAsync<DbUser>
+
+        return insertedCount
+    }
+
+///Handler stuff
+let insertUserHandler : HttpHandler =
+    let handleOk (user : NewUser) : HttpHandler = fun ctx ->
+        task {
+            let! count = insertUser user
+
+            let message = sprintf "Updated Records: %d" count
+            return Response.ofPlainText message ctx
+        }
+
+    Request.mapJson handleOk
 
 let form =
     Templates.html5 "en" [] [
@@ -19,12 +81,14 @@ let wapp = WebApplication.Create()
 let endpoints =
     [
         get "/" (Response.ofPlainText "Hello from /")
-        all "/form" [
+        all "/user" [
             GET, Response.ofHtml form
-            POST, Response.ofEmpty ]
+            POST, insertUserHandler ]
     ]
 
 SQLitePCL.raw.SetProvider(new SQLitePCL.SQLite3Provider_e_sqlite3());
+Dapper.FSharp.SQLite.OptionTypes.register()
+Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true |> ignore
 
 let upgrader = 
     DeployChanges.To.SqliteDatabase("Data Source=./falco.db").WithScriptsFromFileSystem("./Scripts").WithTransaction().LogToConsole().Build()
