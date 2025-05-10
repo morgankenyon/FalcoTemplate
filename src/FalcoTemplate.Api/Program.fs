fba868cec78bbf9191bb45a9ddf96e89b7d36fb4
Morgan Kenyon <morganwkenyon@gmail.com>
2025-05-09 20:01:12 -0500

    Having a simple submission form

--- src/FalcoTemplate.Api/Program.fs
+++ src/FalcoTemplate.Api/Program.fs
@@ -1,19 +1,25 @@
 open Falco
+open Falco.Markup
 open Falco.Routing
 open Microsoft.AspNetCore.Builder
 // ^-- this import adds many useful extensions
 
-let endpoints =
-    [
-        get "/" (Response.ofPlainText "Hello World!")
-        // ^-- associate GET / to plain text HttpHandler
-    ]
+let form =
+    Templates.html5 "en" [] [
+        Elem.form [ Attr.method "post" ] [
+            Elem.input [ Attr.name "name" ]
+            Elem.input [ Attr.type' "submit" ] ] ]
 
 let wapp = WebApplication.Create()
 
+let endpoints =
+    [
+        get "/" (Response.ofPlainText "Hello from /")
+        all "/form" [
+            GET, Response.ofHtml form
+            POST, Response.ofEmpty ]
+    ]
+
 wapp.UseRouting()
-    .Use(DefaultFilesExtensions.UseDefaultFiles)
-    .Use(StaticFileExtensions.UseStaticFiles)
-      // ^-- most IApplicationBuilder extensions are available as static methods similar to this
     .UseFalco(endpoints)
-    .Run(Response.ofPlainText "Not found")
+    .Run()
